4ac3419d22138e508fd45e2bea4648dabfa94b9c
Ankit Jena <Ankit Jena>
2016-05-28 19:48:22 +0530

    Modified to remove all whitespaces from the contents of a file

--- Hashing.java
+++ Hashing.java
@@ -1,5 +1,7 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -7,7 +9,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class Hashing {
 	
-	public static String checkSum(String path) throws NoSuchAlgorithmException, IOException
+	public static void checkSum(String path) throws NoSuchAlgorithmException, IOException
 	{
 		String checksum = null;
 		try 
@@ -15,12 +17,24 @@ public class Hashing {
 			FileInputStream in = new FileInputStream(path);
 			MessageDigest md = MessageDigest.getInstance("MD5");
 			
-			byte[] buffer = new byte [8192];
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			String line = null;
+			StringBuffer fileContents = new StringBuffer("\0");
+			while((line = br.readLine())!= null)
+			{
+				fileContents.append(line);
+			}
+			String file = fileContents.toString();
+			System.out.println(file);
+			file = file.replaceAll("[ \t\n]","");
+			System.out.println(file);
+			byte[] buffer = file.getBytes("UTF-8");
+			 /*
 			int numOfBytesRead;
 			while((numOfBytesRead = in.read(buffer)) > 0)
 			{
 				md.update(buffer, 0, numOfBytesRead);
-			}
+			}*/
 			byte[] hash = md.digest();
 			checksum = new BigInteger(1, hash).toString(16);
 			
@@ -31,7 +45,7 @@ public class Hashing {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return checksum;
+		/*return checksum;*/
 		
 	}
 	
@@ -39,8 +53,9 @@ public class Hashing {
 	{
 		String filePath = "testfile.txt";
 		String filePath1 = "testfile1.txt";
-		System.out.println("Checksum = " + checkSum(filePath));
-		System.out.println("Checksum2 = " + checkSum(filePath1));
+		checkSum(filePath);
+		/*System.out.println("Checksum = " + checkSum(filePath));
+		System.out.println("Checksum2 = " + checkSum(filePath1));*/
 	}
 
 }
